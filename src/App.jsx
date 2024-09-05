0e5f506025f10e27f4d4d61962e5d328d8e993bf
faizal <faizyek0@gmail.com>
2024-09-05 14:48:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import {NavBar1} from './navbar';
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <NavBar1/>
+      <h1>Hello Prosevo</h1>
+      <div></div>
+    </div>
+    <div></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/navbar.jsx
@@ -0,0 +1,28 @@
+import './navbar.css'
+
+function NavBar() {
+  return (
+    <>
+    <div className='navbar'>
+      <h1>Hello Prosevo</h1>
+      <div></div>
+    </div>
+    <div></div>
+    </>
+  );
+}
+
+
+function NavBar1() {
+    return (
+        <>
+    <div className='navbar'>
+      <h1>Hello Prosevo</h1>
+      <div></div>
+    </div>
+    <div></div>
+    </>
+  );
+}
+
+export  {NavBar1,NavBar};
